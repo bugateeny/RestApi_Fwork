35b2ba70edcf70fde8cd57a004eb410070d07f12
adey.sogbesan <bugatiny@gmail.com>
2021-01-15 01:25:39 +0000

    Update to test runner

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -5,9 +5,22 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/main/resources/features_Files",
-        plugin = {"html:target/ReportsDestinations","pretty"},
+@CucumberOptions(features = "src/main/resources/features_Files", monochrome = true,
+        plugin = {
+                "pretty",
+                "html:target/cucumber.html",
+                "json:target/cucumber/cucumber.json",
+                "junit:target/cucumber.xml",
+        },
         tags = "@Regression", glue = {"step_Definitions"})
+
+
+
+
+//@RunWith(Cucumber.class)
+//@CucumberOptions(features = "src/main/resources/features_Files",
+//        plugin = {"html:target/ReportsDestinations","pretty"},
+//        tags = "@Regression", glue = {"step_Definitions"})
 
 public class TestRunner {
 
